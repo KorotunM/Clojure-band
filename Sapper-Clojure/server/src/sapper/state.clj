ec13c42b07413a7df4c45652512c1095bb83eb75
ChursinAlexUnder <keks63620@gmail.com>
2025-05-03 01:46:18 +0300

    Fix timer and add containerization

--- Sapper-Clojure/server/src/sapper/state.clj
+++ Sapper-Clojure/server/src/sapper/state.clj
@@ -1,6 +1,6 @@
 (ns sapper.state
   (:require
-   [clojure.core.async :refer [chan go-loop <! put!]]
+  [clojure.core.async :refer [chan go-loop <! timeout alt! close!]]
    [cheshire.core :as json]
    [org.httpkit.server :as http]
    [sapper.game :as game]))
@@ -15,14 +15,42 @@
 ;; Канал для обработки событий от игроков
 (def event-chan (chan))
 
+; канал‑сигнал для остановки таймера
+(defonce exit-ch (atom nil))
+
+;; отсановка таймера
+(defn stop-timer! []
+  (when-let [ch @exit-ch]
+    (close! ch)))
+
 (defn broadcast-state! []
   (let [state @game-state
         message (json/generate-string state)]
-    (println "[broadcast] Отправка состояния игрокам...")
-
+    ;; (println "[broadcast] Отправка состояния игрокам...")
     (doseq [client @clients]
       (http/send! client message))))
 
+;; Запуск цикла таймера, который каждую секунду уменьшает time-left
+(defn start-timer! []
+  (let [ch (chan)]
+    (reset! exit-ch ch)
+    (go-loop []
+      (alt!
+        ch ([_])
+        (timeout 1000) ([_]
+          (dosync
+            (when (pos? (:time-left @game-state))
+              (alter game-state update :time-left dec)))
+          (broadcast-state!)
+          (if (pos? (:time-left @game-state))
+            (recur)
+            (do
+              (dosync
+                (alter game-state
+                       assoc
+                       :status :ended
+                       :winner (game/calculate-winner @game-state)))
+              (broadcast-state!))))))))
 
 (defn init-game-state []
   (dosync
@@ -33,7 +61,8 @@
        :scores {1 0, 2 0}
        :status :playing
        :time-left 120
-       :players []})))
+       :players []}))
+  (start-timer!))
 
 (defn handle-game-event [event]
   (dosync
@@ -41,8 +70,16 @@
       (cond
         (= action "open") (alter game-state game/open-cell x y player)
         (= action "flag") (alter game-state game/toggle-flag x y player)
-        (= action "restart") (alter game-state (fn [_] (game/new-game 20 20 40)))
+        (= action "restart") 
+        (do
+          (when-let [ch @exit-ch] (close! ch))
+          (init-game-state))
         :else nil)))
+  ;; для остановки таймера при завершении игры 
+  ;; (если будет больше 2 ироков, возможно нужно будет изменить)
+  (when (= (:status @game-state) :ended)
+    (stop-timer!))
+  ;; 
   (broadcast-state!))
 
 ;; Обработчик событий из event-chan
